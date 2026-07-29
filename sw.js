e55be91121632525534604fe2a0e0281865e42a8
William <william@adinnovations.nl>
2026-07-29 13:56:57 +0000

    v3.1: packaging deviation reporting (dirty/broken/short) on grower packaging screen, ripples to dispatch exceptions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mb-mockup-v3.0';
+const CACHE = 'mb-mockup-v3.1';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png', 'icon-180.png'];
 
 self.addEventListener('install', e => {
